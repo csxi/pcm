d6e213aa3f764f562c82867542a7ae49c093bef2
Guochengxi <157224@ycgwl.com>
2018-08-06 15:06:38 +0800

    0.1

---
+++ pcm-dao/src/main/java/persistent/UserMapper.java
@@ -0,0 +1,12 @@
+package persistent;
+
+import org.apache.ibatis.annotations.Param;
+import vo.UserVo;
+
+/**
+ * @author <a href="mailto:157224@ycgwl.com">Guo chengxi</a>
+ * @createDate 2018/8/6 14:18
+ */
+public interface UserMapper {
+    void addUser(@Param("user") UserVo user);
+}
---
+++ pcm-dao/src/main/java/vo/UserVo.java
@@ -0,0 +1,37 @@
+package vo;
+
+/**
+ * @author <a href="mailto:157224@ycgwl.com">Guo chengxi</a>
+ * @createDate 2018/8/6 14:06
+ */
+
+public class UserVo {
+    
+    private String userId;
+    private String userName;
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
