64f4d60feefbf7053bd518328f565dc01f744d67
Guochengxi <157224@ycgwl.com>
2018-08-07 17:13:25 +0800

    0.0.2

--- pcm-dao/src/main/java/persistent/UserMapper.java
+++ pcm-dao/src/main/java/persistent/UserMapper.java
@@ -1,12 +1,14 @@
 package persistent;
 
 import org.apache.ibatis.annotations.Param;
+import org.springframework.transaction.annotation.Transactional;
 import vo.UserVo;
 
 /**
  * @author <a href="mailto:157224@ycgwl.com">Guo chengxi</a>
  * @createDate 2018/8/6 14:18
  */
+
 public interface UserMapper {
 
     void addUser( UserVo user);
--- pcm-service/src/main/java/com/service/impl/UserServiceImpl.java
+++ pcm-service/src/main/java/com/service/impl/UserServiceImpl.java
@@ -3,19 +3,23 @@ package com.service.impl;
 import com.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import persistent.UserMapper;
 import vo.UserVo;
+
+import javax.annotation.Resource;
 
 /**
  * @author <a href="mailto:157224@ycgwl.com">Guo chengxi</a>
  * @createDate 2018/8/6 15:32
  */
+
 @Service("userService")
 public class UserServiceImpl implements IUserService{
 
-    @Autowired
+    @Resource
     private UserMapper userMapper;
-
+    @Transactional
     public void addUser(UserVo user) throws Exception {
         userMapper.addUser(user);
     }
